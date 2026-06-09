e566db77867f747e5ee933774665a83983dc6163
krishna vyas <12krishna.vyas@gmail.com>
2026-06-09 22:57:25 +0530

    Update lambda_function.py

--- AWS/02-serverless-student-management-system/lambda_function.py
+++ AWS/02-serverless-student-management-system/lambda_function.py
@@ -1,1 +1,81 @@
+import json
+import boto3
+import uuid
+from decimal import Decimal
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Students')
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super().default(obj)
+
+def lambda_handler(event, context):
+
+    method = event.get(
+        "requestContext",
+        {}
+    ).get(
+        "http",
+        {}
+    ).get(
+        "method",
+        "POST"
+    )
+
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "*"
+    }
+
+    if method == "OPTIONS":
+        return {
+            "statusCode": 200,
+            "headers": headers,
+            "body": ""
+        }
+
+    if method == "POST":
+
+        body = json.loads(event["body"])
+
+        student = {
+            "id": str(uuid.uuid4()),
+            "name": body["name"],
+            "course": body["course"]
+        }
+
+        table.put_item(Item=student)
+
+        return {
+            "statusCode": 200,
+            "headers": headers,
+            "body": json.dumps({
+                "message": "Student Added",
+                "student": student
+            })
+        }
+
+    elif method == "GET":
+
+        response = table.scan()
+
+        return {
+            "statusCode": 200,
+            "headers": headers,
+            "body": json.dumps(
+                response["Items"],
+                cls=DecimalEncoder
+            )
+        }
+
+    return {
+        "statusCode": 400,
+        "headers": headers,
+        "body": json.dumps({
+            "message": "Invalid Request"
+        })
+    }
